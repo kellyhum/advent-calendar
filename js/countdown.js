95b547d48c7bb861a24550cf8b8c2e2c7e85368a
Kelly H <kellyhum88@gmail.com>
2021-02-10 15:33:21 -0800

    added countdown timer code

--- js/countdown.js
+++ js/countdown.js
@@ -1,14 +1,20 @@
-//Declaring variables for the current date and end date
-const current = new Date().getTime();
-const countdownDate = new Date(2021, 11, 25);
+function calculate() {
+    //Declaring variables for the current date and end date
+    const current = new Date().getTime();
+    const countdownDate = new Date('February 11, 2021, 00:00:00').getTime();
 
-document.write(countdownDate);
+    const difference = countdownDate - current;
 
-//Calculate the time remaining
+    //Converting milliseconds to seconds/minutes/hours/days
+    
+    const seconds = Math.floor((difference/1000) % 60); //1000 milliseconds in a second
+    const minutes = Math.floor((difference/1000/60) % 60); //Divide by 1000 to get seconds, then divide by 60 to get minutes
+    const hours = Math.floor(difference/(60*60*1000) % 24);
+    const days = Math.floor(difference/(24*60*60*1000));
 
-
-//Calculating days, hours, minutes and seconds
-
+    console.log(days + ' days ' + hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds '); 
+}
+calculate();
 
 //Link JS code to HTML document via "dates" class 
 
