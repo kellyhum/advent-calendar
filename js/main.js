017f15423e279a3aee2878ba721eacfd6c4ac17c
Kelly Hum <kellyhum88@gmail.com>
2021-02-16 14:56:44 -0800

    added transition functions

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * main.js
+ * used for the "page" transitions and on-click functions
+ */
+
+//declaring global variables
+const click = document.querySelector(.cards);
+const closingButtons = document.querySelectorAll(button);
+
+//function for the expanding transition
+function expand() {
+
+}
+
+//function for the collapsing transition
+function collapse() {
+
+}
